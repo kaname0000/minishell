05cceaebc7609086fa41865802112f0a999ef983
Okuda Kaname <okaname@c4r3s10.42tokyo.jp>
2025-03-03 15:03:47 +0900

    test

--- main.c
+++ main.c
@@ -6,14 +6,34 @@
 /*   By: okaname <okaname@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 14:20:53 by okaname           #+#    #+#             */
-/*   Updated: 2025/03/03 14:23:05 by okaname          ###   ########.fr       */
+/*   Updated: 2025/03/03 14:54:40 by okaname          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+// #include "minishell.h"
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int	main(void)
 {
-	printf("hello minishell\n");
+	char	*line;
+
+	line = NULL;
+	while (1)
+	{
+		line = readline("> ");
+		if (line == NULL || strlen(line) == 0)
+		{
+			free(line);
+			break ;
+		}
+		printf("line is '%s'\n", line);
+		add_history(line);
+		free(line);
+	}
+	printf("exit\n");
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,13 +6,15 @@
 /*   By: okaname <okaname@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 14:21:50 by okaname           #+#    #+#             */
-/*   Updated: 2025/03/03 14:24:06 by okaname          ###   ########.fr       */
+/*   Updated: 2025/03/03 14:54:17 by okaname          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "libft/libft.h"
 
 #endif
