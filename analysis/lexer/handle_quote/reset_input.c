e2703ad472eade9f48dcd9eb437c1f36fa00d60a
Yota Okamoto <yookamot@c5r1s1.42tokyo.jp>
2025-05-01 22:03:19 +0900

    どうかね

--- analysis/lexer/handle_quote/reset_input.c
+++ analysis/lexer/handle_quote/reset_input.c
@@ -6,7 +6,7 @@
 /*   By: yookamot <yookamot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 19:26:44 by yookamot          #+#    #+#             */
-/*   Updated: 2025/04/29 22:15:36 by yookamot         ###   ########.fr       */
+/*   Updated: 2025/05/01 22:01:39 by yookamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,31 @@ static int	check_input_update(t_tokenset *tokenset, int i, int key)
 		return (SUCCESS);
 }
 
+static void	reset_input_ex(t_tokenset *tokenset, int i, int j)
+{
+	size_t	len;
+	char	*pre;
+	char	*inner;
+	char	*post;
+	char	*tmp;
+
+	len = ft_strlen(tokenset->input);
+	pre = ft_substr(tokenset->input, 0, j);
+	inner = ft_substr(tokenset->input, j + 2, ft_strlen(tokenset->token[i
+				+ 2]->value));
+	post = ft_substr(tokenset->input, j + 2 + ft_strlen(tokenset->token[i
+				+ 2]->value), len - (j + 2 + ft_strlen(tokenset->token[i
+					+ 2]->value)));
+	tmp = ft_strjoin(pre, inner);
+	free(pre);
+	free(inner);
+	pre = ft_strjoin(tmp, post);
+	free(tmp);
+	free(post);
+	free(tokenset->input);
+	tokenset->input = pre;
+}
+
 // tokenのほうでquoteを消すのに応じて、inputのquoteを削除
 void	reset_input(t_tokenset *tokenset, int i, int j, int key)
 {
@@ -32,6 +57,8 @@ void	reset_input(t_tokenset *tokenset, int i, int j, int key)
 
 	if (!check_input_update(tokenset, i, key))
 		return ;
+	if (tokenset->input[j] == tokenset->input[j + 1])
+		return (reset_input_ex(tokenset, i, j));
 	len = ft_strlen(tokenset->input);
 	pre = ft_substr(tokenset->input, 0, j);
 	inner = ft_substr(tokenset->input, j + 1, ft_strlen(tokenset->token[i
