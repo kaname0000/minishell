fb8158241828670270b6256ad751931e4610da29
Yota Okamoto <yookamot@c4r2s12.42tokyo.jp>
2025-05-28 17:47:12 +0900

    echo

--- analysis/lexer/get_tokentype.c
+++ analysis/lexer/get_tokentype.c
@@ -6,7 +6,7 @@
 /*   By: yookamot <yookamot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 13:11:59 by yookamot          #+#    #+#             */
-/*   Updated: 2025/05/24 17:35:35 by yookamot         ###   ########.fr       */
+/*   Updated: 2025/05/28 17:44:50 by yookamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void	get_tokentype2(t_token *token)
 		token->type = TOK_BUILTIN;
 	else
 	{
-		if (!ft_strcmp(token->value, "$NOVAL"))
+		if (token->value[0] == '$')
 		{
 			free(token->value);
 			token->value = ft_strdup("");
